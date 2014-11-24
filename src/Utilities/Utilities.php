ffefff8983fd2f06d32b906b9f60ee2dfade5606
Joe <joe@starboardappdev.com>
2014-11-24 13:45:49 -0500

    Adding tests and they passed

--- src/Utilities/Utilities.php
+++ src/Utilities/Utilities.php
@@ -2,23 +2,37 @@
 
 class Utilities 
 {
-    
-    function __construct()
-    {
-    }
 
+    /**
+     * Default time format.
+     * 
+     * @param  String $date
+     * @return String
+     */
     public static function datetimeFormat($date)
     {
         $date = new DateTime($date);
-        return $date->format('Y-m-d');
+        return $date->format('Y/m/d H:i:s');
     }
 
+    /**
+     * Format a datetime string with no date.
+     * 
+     * @param String $date
+     * @return String
+     */
     public static function datetimeFormatNoDate($date)
     {
         $date = new DateTime($date);
         return $date->format('H:i:s');
     }
 
+    /**
+     * Format a datetime string with no time.
+     * 
+     * @param String $date
+     * @return String
+     */
     public static function datetimeFormatNoTime($date)
     {
         $date = new DateTime($date);
--- tests/UtilitiesTest.php
+++ tests/UtilitiesTest.php
@@ -3,26 +3,50 @@
 class UtilitiesTest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Date used for tests
+     * @var String $date
+     */
     private $date = null;
 
+    /**
+     * Set timezone set and default date string.
+     */
     public function setUp()
     {
         date_default_timezone_set('America/New_York');
-        $this->$date = '2014-12-25 19:16:36';
+        $this->date = '2014-12-25 01:00:00';
     }
 
+    /**
+     * Test default date format.
+     */
     public function testDatetimeFormat()
     {
-        $this->assertEquals('2014-12-25', Utilities::datetimeFormat($this->date));
+        $this->assertEquals('2014/12/25 01:00:00', Utilities::datetimeFormat($this->date));
     }
 
+    /**
+     * Test formatting a date string with only the time.
+     */
     public function testDatetimeFormatNoDate()
     {
-        $this->assertEquals('19:16:36', Utilities::datetimeFormatNoDate($this->date));
+        $this->assertEquals('01:00:00', Utilities::datetimeFormatNoDate($this->date));
     }
 
+    /**
+     * Test formatting a date string with only the time.
+     */
     public function testDatetimeFormatNoTime()
     {
         $this->assertEquals('2014/12/25', Utilities::datetimeFormatNoTime($this->date));
+    }
+
+    /**
+     * Test that formatting works given a different format.
+     */
+    public function testDatetimeFormatWithDifferentDateFormat()
+    {
+        $this->assertEquals('2014/12/25', Utilities::datetimeFormatNoTime('2014-12-25'));
     }
 }
